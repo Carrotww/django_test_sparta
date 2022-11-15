8007cc469b54007ce5c06db3c2766c23be286dbe
Carrotww <rjgjfl90@naver.com>
2022-11-15 09:32:45 +0900

    완료

--- ai2_back/users/tests.py
+++ ai2_back/users/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+class UserRegistrationTest(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username": "deft",
+            "password": "deft",
+            "email": "deft@naver.com",
+            "fullname": "deft",
+            }
+        response = self.client.post(url, user_data)
+        
+        self.assertEqual(response.data['message'], "가입 완료!!")
