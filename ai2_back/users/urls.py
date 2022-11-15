7c40402136cc0730c7ffd99c4e850c7817c381ab
Carrotww <rjgjfl90@naver.com>
2022-11-15 09:11:09 +0900

    commit

--- ai2_back/users/urls.py
+++ ai2_back/users/urls.py
@@ -8,8 +8,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     # user/
-    path('signup/', views.UserView.as_view(), name='user_view'),
-    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', views.UserView.as_view()),
+    path('api/token/', views.SpartaTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
  
 ]
